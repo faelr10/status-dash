16d5b5a01b140f83cec5001e63c03992ce358f4b
Rafael <rafaelv.boscato@hotmail.com>
2024-06-29 14:21:48 -0300

    updates

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ObrasModule } from './obras/obras.module';
 import { FuncionariosModule } from './funcionarios/funcionarios.module';
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [ObrasModule, FuncionariosModule],
   controllers: [],
-  providers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
