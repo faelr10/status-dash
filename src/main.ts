9e6f3e497d0b5550b05c179b9f5ee9b2925b85e4
Rafael <rafaelv.boscato@hotmail.com>
2024-07-15 18:27:36 -0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,16 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configuração específica de CORS para permitir requisições do frontend
   app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: '*',
+    origin: 'https://front-status-dash.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   });
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
   await app.listen(3000);
 }
 bootstrap();
